7351bf727b27f027f1be5bbff1a563ebdc540985
AayushGithub <aayushgandhi2001@gmail.com>
2023-04-11 18:22:34 -0400

    merge issues fixed

--- routes/classRoutes.js
+++ routes/classRoutes.js
@@ -1,5 +1,3 @@
-<<<<<<< Updated upstream
-=======
 /**
  * Class model
  * @typedef {object} Class
@@ -11,13 +9,10 @@
  * @property {string} instructor - The name of the instructor of the class.
  */
 
->>>>>>> Stashed changes
 const express = require("express");
 const classModel = require("../models/classModel");
 const router = express.Router();
 
-<<<<<<< Updated upstream
-=======
 
 /**
  * GET /classes
@@ -48,7 +43,6 @@ const router = express.Router();
  *  "message": "Internal server error"
  * }
  */
->>>>>>> Stashed changes
 router.get("/", async (req, res) => {
     try {
         const classes = await classModel.find();
@@ -59,8 +53,6 @@ router.get("/", async (req, res) => {
 }
 );
 
-<<<<<<< Updated upstream
-=======
 /**
  * GET /classes/{id}
  * @summary Gets the class with the specified ID
@@ -87,80 +79,11 @@ router.get("/", async (req, res) => {
  *  "message": "Internal server error"
  * }
  */
->>>>>>> Stashed changes
 router.get("/:id", getClass, (req, res) => {
     res.json(res.class);
 }
 );
 
-<<<<<<< Updated upstream
-router.post("/", async (req, res) => {
-    const classObject = new classModel({
-        name: req.body.name,
-        description: req.body.description,
-        number: req.body.number,
-        section: req.body.section,
-        semester: req.body.semester,
-        year: req.body.year,
-        instructor: req.body.instructor,
-    });
-    try {
-        const newClass = await classObject.save();
-        res.status(201).json(newClass);
-    }
-    catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-}
-);
-
-router.patch("/:id", getClass, async (req, res) => {
-    if (req.body.name == null) {
-        return res.status(400).json({ message: "Name is required" });
-    }
-    if (req.body.number == null) {
-        return res.status(400).json({ message: "Number is required" });
-    }
-    if (req.body.section == null) {
-        return res.status(400).json({ message: "Section is required" });
-    }
-    if (req.body.semester == null) {
-        return res.status(400).json({ message: "Semester is required" });
-    }
-    if (req.body.year == null) {
-        return res.status(400).json({ message: "Year is required" });
-    }
-    if (req.body.name != null) {
-        res.class.name = req.body.name;
-    }
-    if (req.body.description != null) {
-        res.class.description = req.body.description;
-    }
-    if (req.body.number != null) {
-        res.class.number = req.body.number;
-    }
-    if (req.body.section != null) {
-        res.class.section = req.body.section;
-    }
-    if (req.body.semester != null) {
-        res.class.semester = req.body.semester;
-    }
-    if (req.body.year != null) {
-        res.class.year = req.body.year;
-    }
-    if (req.body.instructor != null) {
-        res.class.instructor = req.body.instructor;
-    }
-    try {
-        const updatedClass = await res.class.save();
-        res.json(updatedClass);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-}
-);
-
-=======
 /**
  * POST /classes
  * @summary Create a new class
@@ -333,7 +256,6 @@ router.patch("/:id", getClass, async (req, res) => {
  *   "message": "Internal server error"
  * }
  */
->>>>>>> Stashed changes
 router.delete("/:id", async (req, res) => {
     try {
         const classMade = await classModel.findByIdAndRemove(req.params.id);
